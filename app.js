7eb5037a0e8991410a9d68aa066d4d7beec264c0
Bartosz <bartek@gmail.com>
2019-01-29 12:54:36 +0100

    const defined in js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log(123);
+const form = document.querySelector('#form');
+const inputEnterTask = document.querySelector('#task-input');
+const list = document.querySelector('.list');
+const searchInput = document.querySelector('#filter');
+const clearBtn = document.querySelector('.delete-all-items');
