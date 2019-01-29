0f7fff2606da36e57d3d830e74858fdd6ec9b8b3
Bartosz <bartek@gmail.com>
2019-01-29 12:59:52 +0100

    evetsList function added in js

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const inputEnterTask = document.querySelector('#task-input');
 const list = document.querySelector('.list');
 const searchInput = document.querySelector('#filter');
 const clearBtn = document.querySelector('.delete-all-items');
+
+eventsList();
+
+function eventsList(){
+  form.addEventListener('submit', addToDo);
+}
+
+function addToDo(e){
+  console.log('dupa');
+
+  e.preventDefault();
+}
